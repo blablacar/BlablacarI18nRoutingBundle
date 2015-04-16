6e507c94bb5144a5dd12c2e5a861a6a84950cbde
Olivier Dolbeau <contact@odolbeau.fr>
2015-04-16 17:33:01 +0200

    Repair tests

--- Tests/Routing/Generator/UrlGeneratorTest.php
+++ Tests/Routing/Generator/UrlGeneratorTest.php
@@ -4,6 +4,7 @@ namespace Blablacar\I18nRoutingBundle\Tests\Routing\Generator;
 
 use Blablacar\I18nRoutingBundle\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\RequestContext;
+use Blablacar\I18nRoutingBundle\Routing\Cache\FileCache;
 
 class UrlGeneratorTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,7 +13,7 @@ class UrlGeneratorTest extends \PHPUnit_Framework_TestCase
      */
     public function testGenerate_withUnknownRoute_throwRoutNotFoundException()
     {
-        $urlGenerator = new UrlGenerator(new RequestContext(), null);
+        $urlGenerator = new UrlGenerator(new RequestContext(), new FileCache(sys_get_temp_dir()));
         $urlGenerator->generate('foobar');
     }
 }
